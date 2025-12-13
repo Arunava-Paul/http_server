41104cb322f5d0f44b2caf4a55240ac250668ba3
Arunava-Paul <arunavapaul1994@gmail.com>
2025-12-13 18:54:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
     bind_addr = "0.0.0.0"
     server = ThreadingHTTPServer((bind_addr, 8888), SimpleHandler)
     print(f"HTTP server started on {bind_addr}:8888 (use your device IP to access)")
-     try:
+    try:
         server.serve_forever()
     except KeyboardInterrupt:
         pass
